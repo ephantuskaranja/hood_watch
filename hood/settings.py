70236351c24b240af1e9f77c24c71476c6efc9a9
Ephantus karanja <ephantusslogan@gmail.com>
2018-08-07 10:38:00 +0300

    add email signup

--- hood/settings.py
+++ hood/settings.py
@@ -122,3 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+EMAIL_USE_TLS=True
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_HOST_USER='testephaapps@gmail.com'
+EMAIL_HOST_PASSWORD='@testapps123'
+EMAIL_PORT=587
