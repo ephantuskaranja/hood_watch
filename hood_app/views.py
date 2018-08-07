9c89471c02433e89ad1aedd778e32199df07e802
Ephantus karanja <ephantusslogan@gmail.com>
2018-08-07 09:42:35 +0300

    create index view function

--- hood_app/views.py
+++ hood_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    context={
+        "title":'Neighbourhood watch'
+    }
+    return render(request, 'index.html', context)
