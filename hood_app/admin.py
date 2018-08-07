8fb4f31f9f088c26f3487f94788c9454ddd38cb8
Ephantus karanja <ephantusslogan@gmail.com>
2018-08-07 17:18:54 +0300

    modify models, add timestamp

--- hood_app/admin.py
+++ hood_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Hood, Recommendation, Reports
 # Register your models here.
+admin.site.register(Hood)
+admin.site.register(Recommendation)
+admin.site.register(Reports)
--- hood_app/models.py
+++ hood_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
 
 # Create your models here.
 class Hood(models.Model):
@@ -14,6 +15,9 @@ class Hood(models.Model):
     occupants_count = models.IntegerField(blank=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
     email=models.EmailField(max_length=30, blank=True, null=True)
+
+    def __str__(self):
+        return self.location
 
 
 class Recommendation(models.Model):
@@ -33,6 +37,14 @@ class Recommendation(models.Model):
     location = models.ForeignKey(Hood,on_delete=models.CASCADE,null=True)
     institution_category = models.CharField(max_length=60,choices=INS_CHOICES)
     outstanding = models.CharField(max_length=60,choices=OUTSTANDING_CHOICES)
+    desc = models.TextField(max_length=250, blank=True)
+    created_at = models.DateField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.institution_name
+
+    class Meta:
+        verbose_name_plural = 'Recommendations'
 
 
 class Reports(models.Model):
@@ -41,7 +53,22 @@ class Reports(models.Model):
         ('BAD-SERVICES', 'Bad-Services'),
         ('BRIBERY', 'Bribery')
     )
+
+    CATEGORIES =(
+        ('PUBLIC', 'public'),
+        ('PRIVATE', 'private')
+        )
     institution_name = models.CharField(max_length=50, blank=True, null=True)
-    dept=models.CharField(max_length=50, blank=True, null=True)
-    location=models.ForeignKey(Hood,on_delete=models.CASCADE,null=True)
-    institution_category=models.CharField(max_length=60, choices=OUTSTANDING_CHOICES)
+    dept = models.CharField(max_length=50, blank=True, null=True)
+    location = models.ForeignKey(Hood,on_delete=models.CASCADE,null=True)
+    outstanding = models.CharField(max_length=60, choices=OUTSTANDING_CHOICES, null=True)
+    institution_category = models.CharField(max_length=60, choices=CATEGORIES, blank=True)
+    desc = models.TextField(max_length=250, blank= True)
+    created_at = models.DateField(default=datetime.now, blank=True)
+
+
+    def __str__(self):
+        return self.outstanding
+
+    class Meta:
+        verbose_name_plural ='Reports'
